1da827fbe82abee34148848bb3db8b952d651889
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2023-12-02 22:02:04 +0100

    Replace routes scanning from fs to Vite

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,27 +1,26 @@
 import type { LayoutServerLoad } from "./$types";
-import * as fs from "fs";
 
 /**
  * Scans the given directory for subroutes. Does not include dynamic routes.
  * @param path the path to scan routes from
  * @returns an array of routes
  */
-function getRoutes(path: string) {
-	const files = fs.readdirSync(path).filter(file => !file.includes("[") && !file.includes("(")); // Filter out dynamic routes
-	const routes: string[] = [];
-	for (const file of files) {
-		const filePath = `${path}/${file}`;
-		if (fs.statSync(filePath).isDirectory()) {
-			routes.push(...getRoutes(filePath));
-		} else if (file === "+page.svelte") {
-			routes.push(filePath.split("/").at(-2) ?? "");
-		}
-	}
-	return routes;
+function getRoutes() {
+	const files = Object.keys(import.meta.glob("/src/routes/**/*")).filter(
+		file => !file.includes("[") && !file.includes("(")
+	);
+	return [
+		...new Set(
+			files
+				.map(file => file.split("/").slice(0, -1).join("/").replace("/src/routes", ""))
+				.filter(Boolean)
+				.map(route => route.replace(/^\//, ""))
+		)
+	];
 }
 
 export const load: LayoutServerLoad = () => {
 	return {
-		years: getRoutes("src/routes")
+		years: getRoutes()
 	};
 };
