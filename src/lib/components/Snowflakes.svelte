7dc38496bf9dee401ea3af14f33ec31af298d3ea
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2023-12-04 21:36:42 +0100

    Improve Snowflakes Now based on the official snippet: https://gist.github.com/khromov/f8bd2d30b14cce7a1dc36149069ad5e6 - Supports window resizing - Supports reduced motion

--- src/lib/components/Snowflakes.svelte
+++ src/lib/components/Snowflakes.svelte
@@ -1,35 +1,57 @@
 <script lang="ts">
-import { onMount } from "svelte";
+import { onDestroy, onMount, tick } from "svelte";
 
-let innerWidth: number;
-let innerHeight: number;
+let width: number;
+let height: number;
 let snowflakes: HTMLCanvasElement;
+let particles: {
+	x: number;
+	y: number;
+	radius: number;
+	density: number;
+}[] = [];
 let timeout: ReturnType<typeof setTimeout>;
 
-onMount(() => {
-	const ctx = snowflakes.getContext("2d")!;
+function resize() {
+	if (!snowflakes) return;
 
-	// Canvas size
-	const WIDTH = innerWidth;
-	const HEIGHT = innerHeight;
-	snowflakes.width = WIDTH;
-	snowflakes.height = HEIGHT;
+	width = window.innerWidth;
+	height = window.innerHeight;
 
-	// Create the particles
+	snowflakes.width = width;
+	snowflakes.height = height;
+}
+
+function debounceResize() {
+	clearTimeout(timeout);
+	timeout = setTimeout(resize, 100);
+}
+
+function regenerateParticles() {
 	const maxParticles = 100;
-	const particles: { x: number; y: number; radius: number; density: number }[] = [];
+
+	const newParticles: typeof particles = [];
 	for (let i = 0; i < maxParticles; i++) {
-		particles.push({
-			x: Math.random() * WIDTH,
-			y: Math.random() * HEIGHT,
+		newParticles.push({
+			x: Math.random() * width,
+			y: Math.random() * height,
 			radius: 1 + Math.random() * 2,
 			density: Math.random() * maxParticles
 		});
 	}
+	return newParticles;
+}
+
+function startSnow() {
+	resize();
+
+	particles = regenerateParticles();
+
+	const ctx = snowflakes.getContext("2d")!;
 
 	// Draw the flakes
 	function draw() {
-		ctx.clearRect(0, 0, WIDTH, HEIGHT);
+		ctx.clearRect(0, 0, width, height);
 
 		for (let particle of particles) {
 			ctx.fillStyle = "rgba(255, 255, 255, 0.4)";
@@ -42,20 +64,21 @@ onMount(() => {
 
 	// Move the snowflakes
 	let angle = 0;
-	let n = 5;
-	function update(d = Date.now()) {
-		angle = d / 3e3;
+	let margin = 50;
+	const speed = 3000;
+	function update(time = Date.now()) {
+		angle = time / speed;
 		for (let [i, particle] of particles.entries()) {
 			// Updating X and Y coordinates
 			particle.y += 1 + Math.cos(angle + particle.density) + particle.radius / 2;
 			particle.x += Math.sin(angle) * 2;
 
 			// Sending flakes back from the top when it exits
-			if (particle.x > WIDTH + n || particle.x < -n || particle.y > HEIGHT) {
+			if (particle.x > width + margin || particle.x < -margin || particle.y > height) {
 				if (i % 3 > 0) {
 					// 2/3 of the flakes
 					particles[i] = {
-						x: Math.random() * WIDTH,
+						x: Math.random() * width,
 						y: -10,
 						radius: particle.radius,
 						density: particle.density
@@ -65,16 +88,16 @@ onMount(() => {
 					if (Math.sin(angle) > 0) {
 						// Enter from the left
 						particles[i] = {
-							x: -n,
-							y: Math.random() * HEIGHT,
+							x: -margin,
+							y: Math.random() * height,
 							radius: particle.radius,
 							density: particle.density
 						};
 					} else {
 						// Enter from the right
 						particles[i] = {
-							x: WIDTH + n,
-							y: Math.random() * HEIGHT,
+							x: width + margin,
+							y: Math.random() * height,
 							radius: particle.radius,
 							density: particle.density
 						};
@@ -83,25 +106,32 @@ onMount(() => {
 			}
 		}
 		requestAnimationFrame(() => {
-			timeout = setTimeout(draw, 1e3 / 60);
+			timeout = setTimeout(draw, 1000 / 60);
 		});
 	}
 
 	// Animation loop
 	draw();
+}
 
-	return () => {
-		clearTimeout(timeout);
-	};
+onMount(async () => {
+	if (window.matchMedia("(prefers-reduced-motion: reduce)").matches) return;
+	await tick();
+
+	startSnow();
+});
+
+onDestroy(() => {
+	clearTimeout(timeout);
 });
 </script>
 
-<svelte:window bind:innerWidth={innerWidth} bind:innerHeight={innerHeight} />
+<svelte:window on:resize={debounceResize} />
 
 <canvas
 	aria-hidden="true"
 	bind:this={snowflakes}
-	width={innerWidth}
-	height={innerHeight}
-	class="pointer-events-none fixed inset-0 !-z-10 h-screen w-screen"
+	width={width}
+	height={height}
+	class="pointer-events-none fixed inset-0 -z-10 h-screen w-screen"
 ></canvas>
