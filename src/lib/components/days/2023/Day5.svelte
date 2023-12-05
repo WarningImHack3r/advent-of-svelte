9db05d8bd796648762b54d53df365fa6f8cd2cbb
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2023-12-06 00:10:25 +0100

    Add day 5

---
+++ src/lib/components/days/2023/Day5.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+import { onDestroy, onMount } from "svelte";
+import { Gauge } from "lucide-svelte";
+import * as Card from "$lib/components/ui/card";
+
+type Task = {
+	elf: string;
+	task: "CREATED_TOY" | "WRAPPED_PRESENT";
+	minutesTaken: number;
+	date: string;
+};
+function stringFromTaskType(task: Task["task"]) {
+	switch (task) {
+		case "CREATED_TOY":
+			return "created a toy";
+		case "WRAPPED_PRESENT":
+			return "wrapped a present";
+	}
+}
+
+let tasks: Task[] = [];
+async function getElfTasks() {
+	return await fetch("https://advent.sveltesociety.dev/data/2023/day-five.json").then(
+		res => res.json() as Promise<Task[]>
+	);
+}
+
+// Reactive variables
+let toysPerHour = "--";
+let minutesPerToy = "--";
+let mostProductiveElf = "--";
+let completionRate = "--";
+let lastUpdate = "loading...";
+$: if (tasks.length > 0) {
+	const totalMinutes = tasks.reduce((acc, task) => acc + task.minutesTaken, 0);
+	const totalToys = tasks.filter(task => task.task === "CREATED_TOY").length;
+	const totalPresents = tasks.filter(task => task.task === "WRAPPED_PRESENT").length;
+	toysPerHour = (totalToys / (totalMinutes / 60)).toFixed(2);
+	minutesPerToy = (totalMinutes / totalToys).toFixed(2);
+	mostProductiveElf = [...new Set(tasks.map(task => task.elf))]
+		.map(elf => ({
+			elf,
+			toys: tasks.filter(task => task.elf === elf && task.task === "CREATED_TOY").length
+		}))
+		.sort((a, b) => b.toys - a.toys)[0]!.elf;
+	completionRate = ((totalPresents / totalToys) * 100).toFixed(2);
+	const latestTask = tasks[tasks.length - 1]!;
+	lastUpdate =
+		new Intl.RelativeTimeFormat("en", {
+			style: "short"
+		}).format(
+			-Math.ceil(new Date().getTime() / 1000 - new Date(latestTask.date).getTime() / 1000),
+			"seconds"
+		) +
+		" - " +
+		latestTask.elf +
+		" " +
+		stringFromTaskType(latestTask.task);
+}
+
+let interval: ReturnType<typeof setInterval>;
+onMount(async () => {
+	tasks = await getElfTasks();
+	interval = setInterval(async () => {
+		tasks = await getElfTasks();
+	}, 1000);
+});
+
+onDestroy(() => {
+	clearInterval(interval);
+});
+</script>
+
+<Card.Root>
+	<Card.Header>
+		<Card.Title class="flex items-center gap-2">
+			<Gauge class="text-primary" />
+			Elf Productivity Dashboard
+		</Card.Title>
+		<Card.Description>
+			Keep an eye on your elves' productivity with this dashboard.
+		</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<!-- Highlighted stats -->
+		<div class="grid grid-cols-2 gap-8">
+			<!-- toys/hour, time/toy, most productive elf, completion rate -->
+			<div class="flex flex-col">
+				<span class="text-4xl font-semibold tracking-tighter">{toysPerHour}</span>
+				<span class="uppercase text-muted-foreground">toys per hour</span>
+			</div>
+			<div class="flex flex-col">
+				<span class="text-4xl font-semibold tracking-tighter">{minutesPerToy}</span>
+				<span class="uppercase text-muted-foreground">minutes per toy</span>
+			</div>
+			<div class="flex flex-col">
+				<span class="text-4xl font-semibold tracking-tighter">{mostProductiveElf}</span>
+				<span class="uppercase text-muted-foreground">most productive elf</span>
+			</div>
+			<div class="flex flex-col">
+				<span class="text-4xl font-semibold tracking-tighter">{completionRate}%</span>
+				<span class="uppercase text-muted-foreground">completion rate</span>
+			</div>
+		</div>
+		<!-- Last activity marquee -->
+		<div
+			class="-mx-6 -mb-6 mt-6 rounded-b-lg border-t bg-background px-3 py-1.5 font-mono text-primary"
+		>
+			Last update: {lastUpdate}
+		</div>
+	</Card.Content>
+</Card.Root>
--- src/routes/2023/+page.svelte
+++ src/routes/2023/+page.svelte
@@ -4,6 +4,7 @@ import Day1 from "$lib/components/days/2023/Day1.svelte";
 import Day2 from "$lib/components/days/2023/Day2.svelte";
 import Day3 from "$lib/components/days/2023/Day3.svelte";
 import Day4 from "$lib/components/days/2023/Day4.svelte";
+import Day5 from "$lib/components/days/2023/Day5.svelte";
 
 export let data: PageData;
 </script>
@@ -12,11 +13,12 @@ export let data: PageData;
 	<h2 class="text-3xl font-bold tracking-tight">Santa's Dashboard</h2>
 	<div
 		class="mt-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4
-		lg:[&>*:nth-child(1)]:col-span-2 lg:[&>*:nth-child(3)]:col-span-2"
+		lg:[&>*:nth-child(odd)]:col-span-2"
 	>
 		<Day1 kids={data.streamed.kids} />
 		<Day2 />
 		<Day3 presents={data.streamed.presents} />
 		<Day4 />
+		<Day5 />
 	</div>
 </div>
